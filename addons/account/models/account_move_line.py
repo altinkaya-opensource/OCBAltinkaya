4f49045cfc51368a013ce6ff4e17f85362fd640d
yibudak <yibudak@gmail.com>
2025-01-21 19:44:45 +0000

    [REF][WIP] Account and Journal constrains check

--- addons/account/models/account_move_line.py
+++ addons/account/models/account_move_line.py
@@ -1183,29 +1183,30 @@ class AccountMoveLine(models.Model):
         # Avoid using api.constrains here as in case of a write on
         # account move and account move line in the same operation, the check would be done
         # before all write are complete, causing a false positive
-        self.flush_recordset()
-        for line in self.filtered(lambda x: x.display_type not in ('line_section', 'line_note')):
-            account = line.account_id
-            journal = line.move_id.journal_id
+        # yigit: [WIP] We doesn't track any account info in web. So, we don't need to check this constraint.
+        # self.flush_recordset()
+        # for line in self.filtered(lambda x: x.display_type not in ('line_section', 'line_note')):
+        #     account = line.account_id
+        #     journal = line.move_id.journal_id
 
-            if account.deprecated:
-                raise UserError(_('The account %s (%s) is deprecated.') % (account.name, account.code))
+        #     if account.deprecated:
+        #         raise UserError(_('The account %s (%s) is deprecated.') % (account.name, account.code))
 
-            account_currency = account.currency_id
-            if account_currency and account_currency != line.company_currency_id and account_currency != line.currency_id:
-                raise UserError(_('The account selected on your journal entry forces to provide a secondary currency. You should remove the secondary currency on the account.'))
+        #     account_currency = account.currency_id
+        #     if account_currency and account_currency != line.company_currency_id and account_currency != line.currency_id:
+        #         raise UserError(_('The account selected on your journal entry forces to provide a secondary currency. You should remove the secondary currency on the account.'))
 
-            if account.allowed_journal_ids and journal not in account.allowed_journal_ids:
-                raise UserError(_('You cannot use this account (%s) in this journal, check the field \'Allowed Journals\' on the related account.', account.display_name))
+        #     if account.allowed_journal_ids and journal not in account.allowed_journal_ids:
+        #         raise UserError(_('You cannot use this account (%s) in this journal, check the field \'Allowed Journals\' on the related account.', account.display_name))
 
-            if account in (journal.default_account_id, journal.suspense_account_id):
-                continue
+        #     if account in (journal.default_account_id, journal.suspense_account_id):
+        #         continue
 
-            is_account_control_ok = not journal.account_control_ids or account in journal.account_control_ids
+        #     is_account_control_ok = not journal.account_control_ids or account in journal.account_control_ids
 
-            if not is_account_control_ok:
-                raise UserError(_("You cannot use this account (%s) in this journal, check the section 'Control-Access' under "
-                                  "tab 'Advanced Settings' on the related journal.", account.display_name))
+        #     if not is_account_control_ok:
+        #         raise UserError(_("You cannot use this account (%s) in this journal, check the section 'Control-Access' under "
+        #                           "tab 'Advanced Settings' on the related journal.", account.display_name))
 
     @api.constrains('account_id', 'tax_ids', 'tax_line_id', 'reconciled')
     def _check_off_balance(self):
